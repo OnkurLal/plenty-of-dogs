d260a250bc5470a423ff897f2747192ee42eab0c
Onkur Lal <onkur24lal@gmail.com>
2023-01-05 23:02:11 -0500

    added search page selection menu

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -43,7 +43,15 @@ export default function Page() {
     };
 
     if (loading) {
-        return <p className='container grid justify-items-center'>Loading...</p>;
+        return (
+        <>
+        <div className="flex justify-center items-center h-screen w-screen">
+        <div className="text-gray-600 w-10 h-10 text-5xl mx-auto">
+        <i className="fas fa-spinner fa-spin animate-spin" />
+        </div>
+        </div>
+        </>
+        );
       }
     
       if (error) {

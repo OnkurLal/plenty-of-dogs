32e52e0e70c81206e9a52cf403c8bbdebdd28b14
Onkur Lal <onkur24lal@gmail.com>
2023-01-06 09:13:13 -0500

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png/"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png/"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
         <title>Pupster App</title>
       </head>
       <body className="">
